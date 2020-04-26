471d3057bbd23d2b70e317a6d2e335b131fc952b
takumi <takumines6@gamil.com>
2020-04-26 17:50:40 +0900

    techpit完了

---
+++ ecsite/app/Http/Controllers/BuyController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\CartItem;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\Buy;
+use Illuminate\Support\Facades\Mail;
+
+class BuyController extends Controller
+{
+    public function index()
+    {
+        $cartitems = CartItem::where('user_id', Auth::id())->get();
+        $subtotal = 0;
+        foreach ($cartitems as $cartitem) {
+            $subtotal += $cartitem->item->amount * $cartitem->quantity;
+        }
+
+        return view('buy.index', ['cartitems' => $cartitems, 'subtotal' => $subtotal]);
+    }
+
+    public function store(Request $request)
+    {
+        if ($request->has('post')) {
+            $cartitems = CartItem::where('user_id', Auth::id())->get();
+            $subtotal = 0;
+            foreach ($cartitems as $cartitem) {
+                $subtotal += $cartitem->item->amount * $cartitem->quantity;
+            }
+            Mail::to(Auth::user()->email)->send(new Buy($cartitems, $subtotal));
+            CartItem::where('user_id', Auth::id())->delete();
+            return view('buy.complete');
+        }
+        $request->flash();
+        return $this->index();
+    }
+}
---
+++ ecsite/resources/views/buy/mail.blade.php
@@ -0,0 +1,36 @@
+<div>
+    これは{{ config('app.name', 'Laravel') }}のテストメールです。
+</div>
+<div>
+    この度はご購入ありがとうございます。
+</div>
+<div>
+    入金が確認されました。
+</div>
+<div>
+    <div class="card">
+        @foreach ($cartitems as $cartitem)
+            <div class="card-header">
+                {{ $cartitem->item->name }}
+            </div>
+            <div class="card-body">
+                <div>
+                    {{ $cartitem->item->amount }}円
+                </div>
+                <div>
+                    {{ $cartitem->quantity }}個
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+<div class="card">
+    <div class="card-header">
+        小計
+    </div>
+    <div class="card-body">
+        <div>
+            {{ $subtotal }}円
+        </div>
+    </div>
+</div>
