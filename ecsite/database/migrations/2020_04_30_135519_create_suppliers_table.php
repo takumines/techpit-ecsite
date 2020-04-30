158d2cef90e7e752ff846f31850df733b5c33e7f
takumines <takumines6@gmail.com>
2020-04-30 23:14:11 +0900

    SuppliersTableSeederの作成

--- ecsite/database/migrations/2020_04_30_135519_create_suppliers_table.php
+++ ecsite/database/migrations/2020_04_30_135519_create_suppliers_table.php
@@ -15,6 +15,11 @@ class CreateSuppliersTable extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
