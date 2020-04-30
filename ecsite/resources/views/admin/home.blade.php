daebb24b70789a77da5011cdc86f272cf82be12d
takumi_kurogi <takumines1@gmail.com>
2020-04-30 16:00:00 +0900

    管理者用のログインページを作成した

--- ecsite/resources/views/admin/home.blade.php
+++ ecsite/resources/views/admin/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    管理者ページにログインしました
                 </div>
             </div>
         </div>
